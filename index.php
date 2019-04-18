159f5ae4c64b16bf265f4b54c329231a081d163a
dwj97130 <ventdylan@gmail.com>
2019-04-18 14:24:41 -0400

    .

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php include'header.php';?>
 <?php include'footer.php';?>
 
-<section>
-	
+<section id="pictures">
+	<h1>Gwadabat</h1>
+	<p>Gwadabat l'entreprise numéro 1 du BTP</p>
+	<figure>
+		<img src="bricks" alt="Brick"/>
+		<figcaption>Briques en ciment</figcaption>
+	</figure>
 </section>
