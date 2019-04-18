7d9bf55f76e17cbb7a2ef2d7f89c00f451520410
FrancoisePr <francoise.provot@gmail.com>
2019-04-18 15:09:32 -0400

    fontawesome

--- header.php
+++ header.php
@@ -4,12 +4,13 @@
 	<meta charset="UTF-8">
 	<title>Projet jouet</title>
 	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/all.css">
 </head>
 <body>
 	<header>
 		<nav>
 			<ul>
-				<li><a href="#"><i class="fas fa-home" active></i></a></li>
+				<li><a href="#" active><i class="fas fa-home">Accueil</i></a></li>
 				<li><a href="#">Articles</a></li>
 				<li><a href="#">Commandes</a></li>
 				<li><a href="#">Clients</a></li>
