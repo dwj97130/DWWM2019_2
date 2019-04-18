992912717f74f47f4f38373cdc4c670af6adc83d
FrancoisePr <francoise.provot@gmail.com>
2019-04-18 14:03:14 -0400

    new file:   header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Projet jouet</title>
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="#"></a></li>
+				<li><a href="#">Articles</a></li>
+				<li><a href="#">Commandes</a></li>
+				<li><a href="#">Clients</a></li>
+			</ul>
+		</nav>
+	</header>
