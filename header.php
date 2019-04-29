92fcc9c93ea03e9b279fece3f6173841e07b84b2
FrancoisePr <francoise.provot@gmail.com>
2019-04-29 13:12:47 -0400

    title onglet

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-	<title>Projet jouet</title>
+	<title>Gwadabat</title>
 	<link rel="stylesheet" href="css/styles.css">
 	<link rel="stylesheet" href="css/all.css">
 </head>
